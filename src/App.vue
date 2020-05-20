485fa463a3f50529cbd8487d178c6ee6a38375e9
adler.shimon@gmail.com <adler.shimon@gmail.com>
2020-05-19 23:47:34 -0400

    b4 adding vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Dashboard />
+  </div>
 </template>
 
 <script>
+import Dashboard from "@/views/Dashboard";
 export default {
   name: "App",
-  components: {}
+  components: {
+    Dashboard,
+  },
 };
 </script>
 
